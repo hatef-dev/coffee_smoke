34b4ad09f37e80e2dc3d2de9b97a50d5c425e478
unknown <hatef.dev21@gmail.com>
2025-12-24 18:01:03 +0330

    add twinPelin

--- src/glsl/vertex.glsl
+++ src/glsl/vertex.glsl
@@ -1,12 +1,14 @@
-mat2 rotate2D(float angle) {
+vec2 rotate2D(vec2 value, float angle) {
     float s = sin(angle);
     float c = cos(angle);
     return mat2(
-        c, -s,
-        s,  c
-    );
+        c, s,
+        -s,  c
+    ) * value;
 }
 
+uniform float uTime;
+uniform sampler2D uPerlinNoise;
 
 // varying means that we can send this to the fragment shader
 varying vec2 vUv;
@@ -14,7 +16,16 @@ varying vec2 vUv;
 
 
 void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec3 newPosition = position; 
+
+    float twinPelin = texture(uPerlinNoise, vec2(0.5, uv.y * 0.25 - uTime * 0.005)).r;
+
+    float angle = twinPelin * 10.0;
+    newPosition.xz = rotate2D(newPosition.xz, angle);
+
+
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
     // we send uv to the fragment shader
     vUv = uv;
 }
