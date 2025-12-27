f53a17d939c4b889018777f25c06d6755e2f9717
unknown <hatef.dev21@gmail.com>
2025-12-27 10:42:36 +0330

    practies

--- src/glsl/fragment.glsl
+++ src/glsl/fragment.glsl
@@ -5,27 +5,24 @@ uniform float uTime;
 varying vec2 vUv;
 
 void main() {
-
     vec2 smokeUv = vUv;
     smokeUv.y *= 0.3;
-    smokeUv.x *= 0.5;
-    smokeUv.y +=  -uTime * 0.03;
+    smokeUv.x *= 0.5; 
+    smokeUv.y += -uTime * 0.05;
 
-
-
-
-    // smoke 
     float smoke = texture(uPerlinNoise, smokeUv).r;
 
-    smoke = smoothstep(0.4, 1.0, smoke);
-
+    smoke *= smoothstep(0.4, 1.0, smoke);
+    // smoke = 1.0; 
     smoke *= smoothstep(0.0, 0.1, vUv.x);
-    smoke *= smoothstep(1.0, 0.9, vUv.x);
+    smoke *= smoothstep(1.0 , 0.9, vUv.x);
     smoke *= smoothstep(0.0, 0.1, vUv.y);
-    smoke *= smoothstep(1.0, 0.9, vUv.y);
+    smoke *= smoothstep(1.0 , 0.9, vUv.y);
+
+    
 
     gl_FragColor = vec4(0.6, 0.3, 0.2, smoke);
-    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, smoke);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
